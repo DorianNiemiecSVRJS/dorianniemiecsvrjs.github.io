e0016f05e57c4e6886293c026ab30cb2bb317658
Dorian Niemiec <dorian.niemiec@svrjs.org>
2025-03-24 19:10:24 +0100

    chore: init

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  // ...
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+  ],
+  vite: {
+    ssr: {
+      noExternal: ['@fontsource/geist-sans'],
+    }
+  },
+  site: 'https://dorianniemiecsvrjs.github.io'
+});
